9876311bdc6ad433ac2f7c46ac77a952ce469637
Saleel <saleel@saleel.xyz>
2024-01-21 23:35:28 +0530

    chore: update packages

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,35 +1,49 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
-import commonjs from "vite-plugin-commonjs";
-import 'rollup-plugin-node-polyfills';
+import tsconfig from "vite-tsconfig-paths";
+import "rollup-plugin-node-polyfills";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  root: "./",
-  build: {
-    outDir: "./dist",
-  },
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    commonjs(),
-  ],
-  resolve: {
-    alias: {
-      timers: "rollup-plugin-node-polyfills/polyfills/timers",
-      util: "rollup-plugin-node-polyfills/polyfills/util",
-      constants: "rollup-plugin-node-polyfills/polyfills/constants",
-      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
-      os: "rollup-plugin-node-polyfills/polyfills/os",
-      path: "rollup-plugin-node-polyfills/polyfills/path",
-      stream: "stream-browserify",
-      crypto: "crypto-browserify",
-      buffer: "buffer",
-      fs: "browserify-fs",
-    },
-  },
+export default {
   server: {
     port: 3000,
   },
-});
+  plugins: [react(), tsconfig()],
+  resolve: {
+    alias: {
+      buffer: "buffer",
+      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+      sys: "util",
+      events: "rollup-plugin-node-polyfills/polyfills/events",
+      stream: "stream-browserify",
+      path: "rollup-plugin-node-polyfills/polyfills/path",
+      querystring: "rollup-plugin-node-polyfills/polyfills/qs",
+      // punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
+      url: "rollup-plugin-node-polyfills/polyfills/url",
+      crypto: "crypto-browserify",
+      // string_decoder: "rollup-plugin-node-polyfills/polyfills/string-decoder",
+      http: "rollup-plugin-node-polyfills/polyfills/http",
+      https: "rollup-plugin-node-polyfills/polyfills/http",
+      os: "rollup-plugin-node-polyfills/polyfills/os",
+      // crypto: "rollup-plugin-node-polyfills/polyfills/crypto",
+      // crypto: "crypto-browserify",
+      assert: "rollup-plugin-node-polyfills/polyfills/assert",
+      constants: "rollup-plugin-node-polyfills/polyfills/constants",
+      _stream_duplex:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
+      _stream_passthrough:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
+      _stream_readable:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
+      _stream_writable:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
+      _stream_transform:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
+      timers: "rollup-plugin-node-polyfills/polyfills/timers",
+      console: "rollup-plugin-node-polyfills/polyfills/console",
+      vm: "rollup-plugin-node-polyfills/polyfills/vm",
+      zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
+      tty: "rollup-plugin-node-polyfills/polyfills/tty",
+      domain: "rollup-plugin-node-polyfills/polyfills/domain",
+    },
+  },
+};
