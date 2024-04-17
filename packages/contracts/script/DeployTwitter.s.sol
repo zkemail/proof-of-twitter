a099d3c2de0806e5b980bae8bdda17d1c1e6fc9a
Saleel <saleel@saleel.xyz>
2024-04-17 10:59:45 +0530

    update deploy script

--- packages/contracts/script/DeployTwitter.s.sol
+++ packages/contracts/script/DeployTwitter.s.sol
@@ -19,18 +19,24 @@ contract Deploy is Script, Test {
 
     function run() public {
         uint256 sk = getPrivateKey();
+        address owner = vm.createWallet(sk).addr;
         vm.startBroadcast(sk);
 
         Verifier proofVerifier = new Verifier();
         console.log("Deployed Verifier at address: %s", address(proofVerifier));
 
-        DKIMRegistry dkimRegistry = new DKIMRegistry(msg.sender);
+        DKIMRegistry dkimRegistry = new DKIMRegistry(owner);
         console.log("Deployed DKIMRegistry at address: %s", address(dkimRegistry));
 
         // x.com hash for selector dkim-202308
         dkimRegistry.setDKIMPublicKeyHash(
             "x.com",
             bytes32(uint256(1983664618407009423875829639306275185491946247764487749439145140682408188330))
+        );
+
+        dkimRegistry.setDKIMPublicKeyHash(
+            "x.com",
+            bytes32(uint256(14900978865743571023141723682019198695580050511337677317524514528673897510335))
         );
 
         ProofOfTwitter testVerifier = new ProofOfTwitter(proofVerifier, dkimRegistry);
